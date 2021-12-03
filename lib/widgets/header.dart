3c2c5d96780f883475462bb7417c26ccdd03f23f
akkaradet60 <akkaradet.ko60@snru.ac.th>
2021-12-03 09:38:19 +0700

    init

---
+++ lib/widgets/header.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class header extends StatelessWidget {
+  const header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('TOT ขอนแก่น', style: TextStyle(color: Colors.blueGrey));
+  }
+}
